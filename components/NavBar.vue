268bd1588cb1c22ebda656c001e9d5cfa611d63d
marcaureldev <ahouandjinoumarcaurel10@gmail.com>
2024-06-17 15:32:21 +0200

    Share your Recipes

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <Disclosure as="nav" class="" v-slot="{ open }">
-        <div class="mx-auto">
+        <div class="mx-auto ">
 
             <div class="relative flex h-16 items-center justify-between">
                 <div class="flex items-center font-bold text-xl">
@@ -38,7 +38,8 @@
         <DisclosurePanel class="lg:hidden ">
             <div class="space-y-1 px-2 pb-3 pt-2">
                 <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
+                    'block rounded-md px-3 py-2 text-base font-medium']"
                     :aria-current="item.current ? 'page' : undefined">{{ item.name }}
                 </DisclosureButton>
             </div>
--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
             </span>
           </h1>
 
-          <p class="text-gray_color text-center md:text-start max-w-40">
+          <p class="text-gray_color text-center text-sm sm:text-[16px] md:text-start max-w-40">
             Explorez une galaxie de saveurs, partagez vos créations gourmandes et connectez-vous avec d'autres
             passionnés de cuisine. Inspirez-vous, échangez des recettes authentiques et embarquez pour une aventure
             culinaire inoubliable chaque jour.
@@ -35,8 +35,20 @@
       </div>
     </section>
 
-    <section>
+    <section class="p-5 py-10">
+      <div class="md:flex gap-8 items-center space-y-5">
+        <img src="../assets/images/share.jpg" alt="Image de recettes" class="w-[18em] sm:w-[22em] md:w-[18em] mx-auto rounded-xl sm:rotate-90 md:rotate-0">
 
+        <div class="text-center space-y-5">
+          <h1 class="text-3xl font-bold">Partager Vos <span class="text-brown_color">Recettes</span></h1>
+          <p class="text-gray_color text-sm sm:text-[16px]">
+            Découvrez un monde de saveurs sur notre plateforme de partage de recettes ! Que vous soyez un chef amateur
+            ou un passionné de cuisine, explorez, créez et partagez vos meilleures recettes avec une communauté de
+            gourmets enthousiastes. Rejoignez-nous pour une expérience culinaire délicieusement inspirante.
+          </p>
+          <Button :button="new_recipes" style="color: #fff;"/>
+        </div>
+      </div>
     </section>
 
   </div>
@@ -48,7 +60,8 @@ export default {
   data() {
     return {
       inscription: 'Inscription',
-      connexion: 'Connexion'
+      connexion: 'Connexion',
+      new_recipes: 'Créer une nouvelle recette',
     }
   }
 
